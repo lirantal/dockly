8b36d92f25e1ede8bcac9eb54f2e8d17f1ca8552
Liran Tal <liran.tal@gmail.com>
2017-04-30 19:51:54 -0400

    chore(lint): cosmetic cleanups

--- dockerRunScript.sh
+++ dockerRunScript.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 currentFilePath=${BASH_SOURCE[0]}
 workingDir="$( dirname "$currentFilePath" )"
 containerId=`cat ${workingDir}/containerId.txt`
+
 docker exec -it "$containerId" /bin/bash
