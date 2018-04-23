c6e81c5a195c0f4bc83947e97534c28c764002de
Israel Roldan <iroldan@schubergphilis.com>
2018-04-23 12:05:04 +0200

    fix(exec): Use `/bin/sh` instead of `/bin/bash`

--- dockerRunScript.sh
+++ dockerRunScript.sh
@@ -4,4 +4,4 @@ currentFilePath=${BASH_SOURCE[0]}
 workingDir="$( dirname "$currentFilePath" )"
 containerId=`cat ${workingDir}/containerId.txt`
 
-docker exec -it "$containerId" /bin/bash
+docker exec -it "$containerId" /bin/sh
