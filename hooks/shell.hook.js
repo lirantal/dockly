d520fca1a12b0e0bdb81c0c1c90979073fba74fc
Liran Tal <liran.tal@gmail.com>
2018-07-17 21:23:33 +0300

    fix(shell): handle exceptions on shell login failures

--- hooks/shell.hook.js
+++ hooks/shell.hook.js
@@ -24,9 +24,20 @@ class hook extends baseWidget() {
     let containerId = this.widgetsRepo.get('containerList').getSelectedContainer()
     if (containerId) {
       let containerIdFile = path.join(__dirname, '/../containerId.txt')
-      fs.writeFile(containerIdFile, containerId, 'utf8', function (err) {
+      fs.writeFile(containerIdFile, containerId, 'utf8', (err) => {
         if (!err) {
-          opn(`${__dirname}/../dockerRunScript.sh`)
+          return opn(`${__dirname}/../dockerRunScript.sh`)
+            .catch((err) => {
+              const actionStatus = this.widgetsRepo.get('actionStatus')
+
+              const title = 'Shell login to container'
+              const message = 'Failed opening shell login for container: ' + containerId + ' - ' + err
+
+              actionStatus.emit('message', {
+                title: title,
+                message: message
+              })
+            })
         }
       })
     }
