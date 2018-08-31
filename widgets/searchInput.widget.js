bed507834c84c81341a200c43bd97bbddf50a58a
Liran Tal <liran.tal@gmail.com>
2018-08-31 17:55:36 +0300

    fix: track the value manually due to buggy blessed lib

--- widgets/searchInput.widget.js
+++ widgets/searchInput.widget.js
@@ -3,6 +3,9 @@
 const EventEmitter = require('events')
 const baseWidget = require('../src/baseWidget')
 
+const ASCII_CHAR_START = 33
+const ASCII_CHAR_END = 126
+
 class myWidget extends baseWidget(EventEmitter) {
   constructor ({ blessed = {}, contrib = {}, screen = {}, grid = {} }) {
     super()
@@ -14,6 +17,8 @@ class myWidget extends baseWidget(EventEmitter) {
     this.toggleVisibility = 0
     this.label = this.getLabel()
     this.widget = this.getWidget()
+
+    this.inputValue = []
   }
 
   getLabel () {
@@ -24,23 +29,42 @@ class myWidget extends baseWidget(EventEmitter) {
     return null
   }
 
+  captureText (key) {
+    if (!key || typeof key !== 'object') {
+      return ''
+    }
+
+    if (key.ch) {
+      this.inputValue.push(key.ch)
+    }
+
+    if (key.sequence) {
+      if (key.name === 'backspace') {
+        this.inputValue.pop()
+      }
+
+      const charactarCode = key.sequence.charCodeAt(0)
+      if (charactarCode >= ASCII_CHAR_START && charactarCode <= ASCII_CHAR_END) {
+        this.inputValue.push(key.sequence)
+      }
+    }
+
+    return this.inputValue.join('')
+  }
+
   init () {
     if (!this.widgetsRepo.has('toolbar')) {
       return null
     }
 
     this.widget.on('keypress', (ch, key) => {
-      // console.log(this.widget.content)
-      // this.emit('keypress', this.widget.getValue())
-
-      if (key.name === 'escape' || key.name === 'return') {
+      if (key.name === 'escape' || key.name === 'return' || key.name === 'enter') {
         this.toggleVisibility = !this.toggleVisibility
         this.widget.clearValue()
         this.widget.destroy()
         this.widgetsRepo.get('containerList').focus()
-        // }
       } else {
-        this.emit('keypress', this.widget.getValue())
+        this.emit('keypress', this.captureText(key))
       }
     })
 
@@ -63,7 +87,7 @@ class myWidget extends baseWidget(EventEmitter) {
   }
 
   getWidget () {
-    return this.grid.gridObj.set(...this.grid.gridLayout, this.blessed.textarea, {
+    return this.grid.gridObj.set(...this.grid.gridLayout, this.blessed.textbox, {
       focused: false,
       border: 'line',
       style: {
