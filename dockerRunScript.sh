b9c95af9112651113c05bb11fd218b46ec7da6f9
Michał Wilski <michal.wilski@gmail.com>
2019-02-14 17:10:21 +0100

    feature(integratedShell): remove unused script

--- dockerRunScript.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-currentFilePath=${BASH_SOURCE[0]}
-workingDir="$( dirname "$currentFilePath" )"
-containerId=`cat ${workingDir}/containerId.txt`
-
-docker exec -it "$containerId" /bin/sh
