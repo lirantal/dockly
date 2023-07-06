563a45e5d3b8f7b903bc61b0443b844fe8b5925c
lbeutlich <beutlichlawrenceiii@gmail.com>
2023-07-05 20:01:45 -0400

    Container Sort
    - Added sort by container status
    - Fixed a bug where the container menu was always showing behind the other container panels

---
+++ src/enum.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ContainerState: {
+        created: "created",
+        restarting: "restarting",
+        running: "running", 
+        removing: "removing",
+        paused: "paused",
+        exited: "exited",
+        dead: "dead",
+      }
+}; 
